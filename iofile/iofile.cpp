93ec104a812e1077e52a5544e6da4d57a3a359d8
rezaikram <muhammadikramriza@gmail.com>
2023-06-19 11:35:20 +0700

    membuka file dengan nama contohfile

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -22,28 +22,3 @@ int main() {
 		//menulis dan memasukkan nilai dari 'baris' ke file
 		outfile << baris << endl;
 	}
-	//selesai daalam menulis sekarang tutup file
-	outfile.close();
-
-	//membuka file dalam mode membaca
-	ifstream inflie;
-	//menunjuk ke sebuah file
-	infile.open("contohfile.txt");
-
-	cout << endl << ">= Membuka dan membaca file " << endl;
-	//jika file ada maka
-	if (infile.is_open())
-	{
-		//melakukan perulangan setiap baris
-		while (getline(infile, baris))
-		{
-			//dan tampilkan disitu
-			cout << baris << '\n';
-		}
-		//tutup file tersebut setelah selesai
-		infile.close();
-	}
-	//jika tidak ditemukan file maka akan menampilkan ini
-	else cout << "Unable to open file";
-	return 0;
-}
